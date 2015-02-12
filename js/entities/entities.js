939dc844e048e4326b564308739fb2596835b913
isai cardoso <isai.cardoe@gmail.com>
2015-02-12 12:28:33 -0800

    video 21 made creeps hit tower

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -43,7 +43,7 @@ game.PlayerEntity = me.Entity.extend({
         }
         //i made the character go left when the left key is pressed
         if(me.input.isKeyPressed("jump") && !this.body.jumping && !this.body.falling){
-            this.jumping = true;
+            this.body.jumping = true;
             this.body.vel.y -= this.body.accel.y * me.timer.tick;
         }
         //i am making the character jump when i press the space button
@@ -119,8 +119,7 @@ game.PlayerBaseEntity = me.Entity.extend({
         this.health = 10;
         this.alwaysUpdate = true;
         this.body.onCollision = this.onCollision.bind(this);
-        console.log("init");
-        this.type = "PlayerBaseEntity";
+        this.type = "PlayerBase";
         
         this.renderable.addAnimation("idle", [0]);
         this.renderable.addAnimation("broken", [1]);
@@ -136,6 +135,10 @@ game.PlayerBaseEntity = me.Entity.extend({
         
         this._super(me.Entity, "update", [delta]);
         return true;
+    },
+    
+    loseHealth: function(damage){
+        
     },
     
     onCollision: function(){
@@ -203,7 +206,13 @@ game.EnemyCreep = me.Entity.extend({
         }]);
         this.health = 10;
         this.alwaysUpdate = true;
-        
+        //this.attacking lets us know if the enemy is currently attacking
+        this.attacking = false;
+        //keeps track of when our creep last creep attcked anything
+        this.lastAttacking = new Date().getTime();
+        //keep track of the last time our creep hit anything
+        this.lastHit = new Date().getTime();
+        this.now = new Date().getTime();
         this.body.setVelocity(3, 20);
         
         this.type = "EnemyCreep";
@@ -214,19 +223,34 @@ game.EnemyCreep = me.Entity.extend({
     },
     
     update: function(delta){
-        
+        this.now = new Date().getTime();
         
         this.body.vel.x -= this.body.accel.x * me.timer.tick;
+        
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
         
         
         this.body.update(delta);
         
         
         
-        this._super(me.Entity, "update", [delta]);
-        
+        this._super(me.Entity, "update", [delta]);  
         return true;
-    }
+    },
+    
+    collideHandler: function(response){
+        if(response.b.type==='PlayerBase'){
+            this.attacking=true;
+            this.lastAttacking=this.now;
+            this.body.vel.x = 0;
+            this.pos.x = this.pos.x + 1;
+            if((this.now-this.lastHit >= 1000)){
+                this.lastHit = this.now;
+                response.b.loseHealth(1);
+            }
+        }
+    }  
+    
 });
 
 game.GameManager = Object.extend({
