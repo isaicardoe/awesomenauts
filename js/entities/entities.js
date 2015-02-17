6ede97a9fa82f54b05f693e395045a73a9e257f3
isai cardoso <isai.cardoe@gmail.com>
2015-02-17 12:29:30 -0800

    almost done with video 22

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -10,6 +10,7 @@ game.PlayerEntity = me.Entity.extend({
                 return(new me.Rect(0, 0, 64, 64)).toPolygon();
             }
         }]);
+       this.type = "PlayerEntity";
        this.body.setVelocity(5, 20);
        //keeps track of which direction your character is going
        this.facing = "right";
@@ -75,6 +76,10 @@ game.PlayerEntity = me.Entity.extend({
         
         this._super(me.Entity, "update", [delta]);
         return true;
+    },
+    
+    loseHealth: function(damage){
+      this.health = this.health - damage;  
     },
     
     collideHandler: function(response){
@@ -247,6 +252,25 @@ game.EnemyCreep = me.Entity.extend({
             this.pos.x = this.pos.x + 1;
             //checks that it has been at least 1sec since creep hit a base
             if((this.now-this.lastHit >= 1000)){
+                //updates the lasthit timer
+                this.lastHit = this.now;
+                //makes the player base call its loseHealth function and passes
+                //damage of 1
+                response.b.loseHealth(1);
+            }
+        }else if (response.b.type==='PlayerEntity'){
+            var xdif = this.pos.x - response.b.pos.x;
+            
+            this.attacking=true;
+            //this.lastAttcking=this.now;
+            this.body.vel.x = 0;
+            //keeps moving the creep to the right to maintain its position
+            if(xdif>0){
+                console.log();
+                 this.pos.x = this.pos.x + 1;
+            }
+            //checks that it has been at least 1sec since creep hit a something
+            if((this.now-this.lastHit >= 1000) && xdif>0){
                 //updates the lasthit timer
                 this.lastHit = this.now;
                 //makes the player base call its loseHealth function and passes
