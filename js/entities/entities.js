bdeb36f73a2e9a1bf1a6f9d29d990d1caee71da3
isai cardoso <isai.cardoe@gmail.com>
2015-02-19 12:29:53 -0800

    done with video 23 i can kill creeps now

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -106,6 +106,28 @@ game.PlayerEntity = me.Entity.extend({
                 this.lastHit = this.now;
                 response.b.loseHealth();
             }
+        }else if(response.b.type==='EnemyCreep'){
+            var xdif = this.pos.x - response.b.pos.x;
+            var ydif = this.pos.y - response.b.pos.y;
+            
+            if (xdif>0){
+                this.pos.x = this.pos.x + 1;
+                if(this.facing==="left"){
+                    this.body.vel.x = 0;
+                }
+            }else{
+                this.pos.x = this.pos.x - 1;
+                if(this.facing==="right"){
+                    this.body.vel.x = 0;
+                }
+            }           
+            if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= 1000
+                    && (Math.abs(ydif) <=40) && 
+                    (((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing==="right"))
+                    ){
+                this.lastHit = this.now;
+                response.b.loseHealth(1);
+            }
         }
     }
 });
@@ -229,7 +251,16 @@ game.EnemyCreep = me.Entity.extend({
     
     },
     
+    loseHealth: function(damage){
+        this.health = this.health - damage;  
+    },
+    
     update: function(delta){
+        console.log(this.health);
+        if(this.health <= 0){
+            me.game.world.removeChild(this);
+        }
+        
         this.now = new Date().getTime();
         
         this.body.vel.x -= this.body.accel.x * me.timer.tick;
