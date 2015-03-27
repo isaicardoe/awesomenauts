959e0f507a0af7e2d4a5b6e23e4e20df58aeb709
isai cardoso <isai.cardoe@gmail.com>
2015-03-27 11:23:53 -0700

    done with video 35 refactored gamemanager

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -8,26 +8,40 @@ game.GameManager = Object.extend({
     
     update: function(){
         this.now = new Date().getTime();
+        this.goldTimerCheck(this.now);
+        this.creepTimerCheck();
         
-        if(game.data.player.dead){
-            me.game.world.removeChild(game.data.player);
-            me.state.current().resetPlayer(10, 0);
-        }
-        
+        return true;
+    },
+    
+    goldTimerCheck: function(){
         if(Math.round(this.now/1000)%20 ===0 && (this.now - this.lastCreep >= 1000)){
             game.data.gold += 1;
             console.log("Current gold: " + game.data.gold);
-        }
-        
+        } 
+    },
+    
+    creepTimerCheck: function(){
         if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
             this.lastCreep = this.now;
             var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
             me.game.world.addChild(creepe, 5);
         }
-        
-        return true;
     }
 });
 
+game.HeroDeathManager = Object.extend({
+    init: function(x, y, settings){
+        this.alwaysUpdate = true;   
+    },
+    
+    update: function(){
+        if(game.data.player.dead){
+            me.game.world.removeChild(game.data.player);
+            me.state.current().resetPlayer(10, 0);
+        }  
+    }
+});
+    
 
 
